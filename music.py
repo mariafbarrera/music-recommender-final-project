03cc30727373b34ab3c727cb96c6b15585e9007c
mariafbarrera <mfbarrerab2@gmail.com>
2020-07-27 16:03:40 -0400

    first commit

---
+++ music.py
@@ -0,0 +1,40 @@
+# MVP: Music Recommender
+
+print("What type of music genre do you like?")
+print("")
+print("")
+print("Choose a number, based on what you like!")
+user_choice = input('''
+    1. Hip Hop
+    2. Rock
+    3. Pop 
+    4. Jazz 
+    5. Classic 
+    6. K-pop
+    7. Country 
+    8. Electronic
+    ''')
+
+if user_choice == "1":
+    print("I would suggest to listen to:")
+    print("")
+    print("Started From The Bottom - Drake")
+elif user_choice == "2":
+    print("Why don't you listen to this?")
+    print("")
+    print("Don't Stop Believin - Journey ")
+elif user_choice == "3":
+    print()
+elif user_choice == "4":
+    print("This a nice song!:")
+    print("")
+    print("The Girl From Ipanema - Stan Getz & Joao Gilberto ")
+# elif user_choice == "5":
+
+# elif user_choice == "6":
+
+# elif user_choice == "7":
+
+# elif user_choice == "8":
+
+# else:
