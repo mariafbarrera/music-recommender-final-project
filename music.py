b3e1892eb238de35bec50da5d98aa518d62c4dd8
mariafbarrera <mfbarrerab2@gmail.com>
2020-07-27 16:37:02 -0400

    updated code

--- music.py
+++ music.py
@@ -24,17 +24,28 @@ elif user_choice == "2":
     print("")
     print("Don't Stop Believin - Journey ")
 elif user_choice == "3":
-    print()
+    print("I know you will like this song:")
+    print("")
+    print("Feeling Good - Michael Bublé")
 elif user_choice == "4":
     print("This a nice song!:")
     print("")
     print("The Girl From Ipanema - Stan Getz & Joao Gilberto ")
-# elif user_choice == "5":
-
-# elif user_choice == "6":
-
-# elif user_choice == "7":
-
-# elif user_choice == "8":
-
-# else:
+elif user_choice == "5":
+    print("Have you listened to this song?")
+    print("")
+    print("Gramophone Waltz - Eugen Doga")
+elif user_choice == "6":
+    print("You may want to dance to this one!")
+    print("")
+    print("How You Like That - BLACKPINK")
+elif user_choice == "7":
+    print("You may like this one:")
+    print("")
+    print("Wagon Wheel - Darius Rucker")
+elif user_choice == "8":
+    print("What about this one?")
+    print("")
+    print("Animals - Martin Garrix")
+else:
+    print("I don't think that is an option...")
